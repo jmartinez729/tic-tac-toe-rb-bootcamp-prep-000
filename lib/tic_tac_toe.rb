de26e8c5168c6dccdfe2d220e347dada4a417104
John Martinez <jmartinez729@gmail.com>
2018-08-23 23:04:17 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,13 @@
+def display_board(board)
+  
+
+WIN_COMBINATIONS = [
+  [0, 1, 2], #Top row
+  [3, 4, 5], #Middle row 
+  [6, 7, 8], #Bottom row
+  [0, 3, 6], #Left column
+  [1, 4, 7], #Middle column 
+  [2, 5, 8], #Right column
+  [0, 4, 8], #Top left to bottom right
+  [2, 4, 6]  #Bottom left to top right
+  ]
