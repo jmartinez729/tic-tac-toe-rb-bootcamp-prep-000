b4cb5803fa0e71d89a0f8fac483020c82fd70bbf
John Martinez <jmartinez729@gmail.com>
2018-08-23 23:32:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,23 +82,23 @@ WIN_COMBINATIONS = [
   [2, 4, 6]  #Bottom left to top right
   ]
   
-  def won?(board)
-    WIN_COMBINATIONS.each { |win_combination|
-    win_index_1 = win_combination[0]
-    win_index_2 = win_combination[1]
-    win_index_3 = win_combination[2]
+def won?(board)
+  WIN_COMBINATIONS.each { |win_combination|
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
     
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+  position_1 = board[win_index_1]
+  position_2 = board[win_index_2]
+  position_3 = board[win_index_3]
     
-    if position_1 == "X" &&
-      position_2 == "X" &&
-      position_3 == "X"
+  if position_1 == "X" &&
+    position_2 == "X" &&
+    position_3 == "X"
       
-      return win_combination
+    return win_combination
       
-    elsif position_1 == "O" &&
+  elsif position_1 == "O" &&
     position_2 == "O" &&
     position_3 == "O"
     
@@ -107,8 +107,7 @@ WIN_COMBINATIONS = [
   }
   
   return false 
-    
-  end 
+end 
   
   
   
