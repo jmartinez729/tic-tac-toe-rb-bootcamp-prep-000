31109c97c0638cf2e4f13b9b73bfb468dd19b4bf
John Martinez <jmartinez729@gmail.com>
2018-08-23 23:20:49 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -19,7 +19,7 @@ def position_taken?(board, index)
 end 
 
 def valid_move?(board, index)
-  if position_taken?(board, index) && index.between? 
+  if position_taken?(board, index) && index.between?(0, 8)
     return false
   else
     return true
