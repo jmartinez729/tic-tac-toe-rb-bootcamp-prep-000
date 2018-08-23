5ac8c4b8241c578bf920d9fd3290afad7b9865ed
John Martinez <jmartinez729@gmail.com>
2018-08-23 23:01:55 +0000

    automatically backed up by learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,10 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2], #Top row
+  [3, 4, 5], #Middle row 
+  [6, 7, 8], #Bottom row
+  [0, 3, 6], #Left column
+  [1, 4, 7], #Middle column 
+  [2, 5, 8], #Right column
+  [0, 4, 8], #Top left to bottom right
+  [2, 4, 6]  #Bottom left to top right
+  ]
