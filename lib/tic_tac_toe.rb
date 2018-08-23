efdd71fc5d2f762b42d5a68fd8d1afc5b3bcfc8a
John Martinez <jmartinez729@gmail.com>
2018-08-23 23:31:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,7 +98,9 @@ WIN_COMBINATIONS = [
       
       return win_combination
       
-    elsif position_1 == "O"
+    elsif position_1 == "O" &&
+    position_2 == "O" &&
+    position_3 == "O"
     
   end 
   
